bf5d53bf444c99471c2defcdb7653d6f9729acfb
Nagitu <denipraa1@gmail.com>
2023-09-06 07:09:50 +0700

    feat : add knex migrate files

---
+++ Backend/db/migrations/20230906000756_tambah_tabel_jurusan.js
@@ -0,0 +1,17 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.createTable('jurusan', table=>{
+    table
+  })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  
+};
